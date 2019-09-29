a3cc9f9b40b2b41679b696b487855d13b04e7d42
Chris <cwill833@gmail.com>
2019-09-29 14:30:47 -0400

    fix spelling error

--- src/components/ThankYou/ThankYou.js
+++ src/components/ThankYou/ThankYou.js
@@ -4,7 +4,7 @@ export const ThankYou = ({submitted, level}) => {
 
     return (
         submitted ?
-        <h2>Thank you for your submittion! You are feeling like a {level} today. See you tomorrow!</h2>
+        <h2>Thank you for your submission! You are feeling like a {level} today. See you tomorrow!</h2>
         :
         null
     )
