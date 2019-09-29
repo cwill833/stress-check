b67812b1c30ce82b24929ba5a9db72334f2a5139
Chris <cwill833@gmail.com>
2019-09-29 15:46:19 -0400

    add a Link component to display if user selects 6 or higher

---
+++ src/components/Links/Links.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export const Links = ({level}) => {
+
+
+
+    return <h2>Thank you for your submission! You seem a bit stressed today with a level of {level}. Check out this <a href="https://www.verywellmind.com/stress-management-4157211" target="_blank" rel="noopener noreferrer">link</a> as it may help. See you tomorrow!</h2>
+}
