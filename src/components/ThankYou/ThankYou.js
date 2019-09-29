edc813eb043694b3ed1d7eb1c9ad302fd70e7a4f
Chris <cwill833@gmail.com>
2019-09-29 15:47:17 -0400

    add Link child into ThankYou component

--- src/components/ThankYou/ThankYou.js
+++ src/components/ThankYou/ThankYou.js
@@ -1,10 +1,14 @@
 import React from 'react'
+import { Links } from '../Links/Links';
 
 export const ThankYou = ({submitted, level}) => {
 
     return (
         submitted ?
-        <h2>Thank you for your submission! You are feeling like a {level} today. See you tomorrow!</h2>
+            level <= 5 ?
+            <h2>Thank you for your submission! You are feeling like a {level} today. See you tomorrow!</h2>
+            :
+            <Links level={level} />
         :
         null
     )
