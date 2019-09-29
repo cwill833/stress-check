dd2ea4c98d27cdddc2244d36eb227820f42ed3c2
Chris <cwill833@gmail.com>
2019-09-29 14:30:28 -0400

    fix spelling error

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,7 +4,7 @@ export const Header = ({user}) => {
 
     return (
         <div>
-            <h1>How stressed are you today {user}?</h1>
+            <h1>How stressed are you today, {user}?</h1>
         </div>
     )
 }
